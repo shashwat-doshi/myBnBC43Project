946b31ff2fee918b81a35b51ecd507c002aac788
Shashwat Doshi <shashwat.doshi@mail.utoronto.ca>
2023-07-24 15:54:44 -0400

    removed unnecessary comments

--- src/com.mysql.cj.jdbc.Driver/app/Main.java
+++ src/com.mysql.cj.jdbc.Driver/app/Main.java
@@ -101,31 +101,8 @@ public class Main {
                 }
             }
 
-            // // Execute a query
-            // System.out.println("Preparing a statement...");
-            // Statement stmt = conn.createStatement();
-            // String sql = "SELECT * FROM Sailors;";
-            // ResultSet rs = stmt.executeQuery(sql);
-
-            // // STEP 5: Extract data from result set
-            // while (rs.next()) {
-            // // Retrieve by column name
-            // int sid = rs.getInt("sid");
-            // String sname = rs.getString("sname");
-            // int rating = rs.getInt("rating");
-            // int age = rs.getInt("age");
-
-            // // Display values
-            // System.out.print("ID: " + sid);
-            // System.out.print(", Name: " + sname);
-            // System.out.print(", Rating: " + rating);
-            // System.out.println(", Age: " + age);
-            // }
-
             System.out.println("Closing connection...");
             mainInput.close();
-            // rs.close();
-            // stmt.close();
             conn.close();
             System.out.println("Success!");
         } catch (SQLException e) {
