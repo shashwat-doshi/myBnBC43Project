5b92e4e6781de2696cefbc0af4475f461f80fbc2
D3nam <rahl302000@gmail.com>
2023-08-07 17:01:36 -0400

    fix bugs

---
+++ src/com.mysql.cj.jdbc.Driver/JDBCMyBnBDrop.sql
@@ -0,0 +1,45 @@
+DROP TABLE Offers;
+
+DROP TABLE Room;
+
+DROP TABLE HotelApartment;
+
+DROP TABLE House;
+
+DROP TABLE Booking;
+
+DROP TABLE Review;
+
+DROP TABLE PaymentInfo;
+
+DROP TABLE Payment;
+
+DROP TABLE Listing;
+
+DROP TABLE Currency;
+
+DROP TABLE Comment;
+
+DROP TABLE PaymentType;
+
+DROP TABLE Amenity;
+
+DROP TABLE Property;
+
+DROP TABLE User;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
